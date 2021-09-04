1dcfbd6175b9fd9bdc5020103318fd8ad77eb8ba
zorro901 <zoro907@gmail.com>
2021-09-05 05:56:21 +0900

    17. custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import { VFC } from 'react'
+import { BrowserRouter ,Route,Switch} from 'react-router-dom'
+import { QueryClient,QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
-function App() {
+const App:VFC =()=> {
   return (
-    <div className="">
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              {/*<MainTask />*/}
+            </Route>
+            <Route exact path="/tags">
+              {/*<MainTag />*/}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   );
 }
